354e051b81aa135fc3adea05e2418603140cb332
lanns250 <374397122@qq.com>
2018-10-29 20:05:17 +0800

    提交修改

--- zhtx/src/main/java/com/group/zhtx/webSocket/WebSocketUtil.java
+++ zhtx/src/main/java/com/group/zhtx/webSocket/WebSocketUtil.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.group.zhtx.message.IMessage;
 import com.group.zhtx.message.MessageFactory;
 import net.sf.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -12,6 +14,8 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 
 public class WebSocketUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(WebSocketUtil.class);
 
     public static WebSocket decodeJosn(ByteBuffer buffer){
 
@@ -33,19 +37,45 @@ public class WebSocketUtil {
 
         String stringResult = buffer.toString();
 
+        if(!validOperateIdIsExit(stringResult)){
+            return new WebSocket(null,null);
+        }
+
         JSONObject jsonObject = JSONObject.fromObject(stringResult);
         int operateId = jsonObject.getInt("operateId");
-        System.out.println("我在这里"+jsonObject+"\t"+stringResult+"operateId="+operateId);
         IMessage IMessage = MessageFactory.newMessageByOperateCode(operateId,stringResult);
-        System.out.println("IMessage="+IMessage);
-        WebSocket webSocket = new WebSocket(operateId, IMessage);
 
+        WebSocket webSocket = new WebSocket(operateId, IMessage);
         return webSocket;
+    }
+
+    public static WebSocket decodeJson(String text){
+
+        //String stringResult = buffer.toString();
+        if(!validOperateIdIsExit(text)){
+            return new WebSocket(null,null);
+        }
+
+        JSONObject jsonObject = JSONObject.fromObject(text);
+        int operateId = jsonObject.getInt("operateId");
+        IMessage IMessage = MessageFactory.newMessageByOperateCode(operateId,text);
+
+        WebSocket webSocket = new WebSocket(operateId, IMessage);
+        return webSocket;
+
     }
 
     public static String encodeJson(WebSocket webSocket){
         String s=new Gson().toJson(webSocket.getIMessage());
         System.out.println(s);
         return s;
+    }
+
+    public static boolean validOperateIdIsExit(String text){
+        int operateIdOfIndex = text.indexOf("operateId");
+        if(operateIdOfIndex < 0){
+            return false;
+        }
+        return true;
     }
 }
